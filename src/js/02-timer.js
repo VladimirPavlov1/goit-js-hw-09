3d0758cb95747207044d7025d1ac970eec3a9c96
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-11-11 10:32:35 +0200

    нарешті розпочав

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,24 @@
+
 import flatpickr from "flatpickr";
 
 import "flatpickr/dist/flatpickr.min.css";
 
 console.log(flatpickr)
+const refs = {
+    inputPiker:document.querySelector('input#datetime-picker'),
+}
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+flatpickr("#datetime-picker", {options});
 
 
 
-
-
-
-
-
-
-
-const teamMeetingDate = new Date();
-console.log(teamMeetingDate);
+flatpickr(refs.inputPiker,{onClose(selectedDates){
+    console.log(selectedDates)}});
