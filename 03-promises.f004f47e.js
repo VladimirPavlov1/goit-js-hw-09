b0a845691ffce81574202e905ddb5091be4a70b3
VladimirPavlov1 <106485364+VladimirPavlov1@users.noreply.github.com>
2022-11-14 13:12:20 +0000

    Deploying to gh-pages from @ VladimirPavlov1/goit-js-hw-09@bdc4cf6ccdc5848afe5f36b087657f0ddc21b2ad 🚀

---
+++ 03-promises.f004f47e.js
@@ -0,0 +1,2 @@
+const e={formEl:document.querySelector(".form"),delayEl:document.querySelector(".js-delay"),stepEl:document.querySelector(".js-step"),amountEl:document.querySelector(".js-amount"),submitEl:document.querySelector(".js-submit")};console.log(e);userAmount=0;
+//# sourceMappingURL=03-promises.f004f47e.js.map
